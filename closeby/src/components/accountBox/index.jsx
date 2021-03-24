8dc7192f4ceeca9c449ee03b20f1c61f2ed888e3
goldabar <bartoszgolda3@gmail.com>
2021-03-24 14:28:11 +0100

    Navbar with logo

--- closeby/src/components/accountBox/index.jsx
+++ closeby/src/components/accountBox/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
 import { LoginForm } from './loginForm';
 import { SignupForm } from './signupForm';
