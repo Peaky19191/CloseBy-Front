0f5b554981644ca96c20c803a6e04a738bd9392e
goldabar <bartoszgolda3@gmail.com>
2021-06-10 13:57:24 +0200

    Info cards styles added

--- closeby/src/pages/home.js
+++ closeby/src/pages/home.js
@@ -3,16 +3,10 @@ import Header from '../components/Header/Header';
 
 export const Home = () => {
     return (
-        /*<div style={{
-            display: 'flex',
-            justifyContent: 'center', 
-            alignItems: 'center', 
-            height: '90vh'
-            }}
-        >
-            <h1>Home</h1>
-        </div>*/
-        <Header />
+        <div>
+            <Header />
+        </div>
+        
     )
 }
 
