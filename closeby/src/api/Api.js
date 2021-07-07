acde6a156251961e050edbe2287d86b5a43be638
kkopec <s15509@pjwstk.edu.pl>
2021-07-07 19:06:24 +0200

    Global Admin User List - Pagination FIX

---
+++ closeby/src/api/Api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import authHeader from "../services/auth-header";
+
+const API_URL = "http://localhost:5000/api/company-admin";
+
+const update = (pageInt, rowsPerPage) => {
+    return axios.get(API_URL + "/list",
+        {
+            headers: authHeader(),
+            params: {
+                page: pageInt,
+                limit: rowsPerPage
+            }
+        },
+    );
+};
+
+export default {
+    update,
+};
