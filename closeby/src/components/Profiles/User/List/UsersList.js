5f3e0e90deabc76a683d99d5aa39c5c1af2707cc
kkopec <s15509@pjwstk.edu.pl>
2021-07-10 17:14:15 +0200

    Users List

---
+++ closeby/src/components/Profiles/User/List/UsersList.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import getUsersList from '../../../../Api/user'
+import useStyles from './styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableFooter from '@material-ui/core/TableFooter';
+
+
+const UsersList = () => {
+    const classes = useStyles();
+    const [users, setUsers] = useState([]);
+
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [count, setCount] = useState(0);
+
+
+    const getList = () => {
+        getUsersList.getUsersList(page, rowsPerPage)
+            .then((response) => {
+                const users = response.data.items;
+                const totalPages = response.data.count;
+
+                setUsers(users);
+                setCount(totalPages);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+
+    useEffect(getList, [page, rowsPerPage]);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+    return (
+        <TableContainer className={classes.tableContainer} component={Paper} elevation={3} >
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow >
+                        <TableCell className={classes.tableCellTitle}>User</TableCell>
+                        <TableCell align="center" className={classes.tableCellTitle}>Email</TableCell>
+                        <TableCell align="center" className={classes.tableCellTitle}>Role</TableCell>
+                        <TableCell align="center" className={classes.tableCellTitle}>Gender</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {users.map((items) => (
+                        <TableRow key={items.email} >
+                            <TableCell component="th" scope="row">
+                                {items.firstName}    {items.lastName}
+                            </TableCell>
+                            <TableCell align="center">{items.email}</TableCell>
+                            <TableCell align="center">{items.role}</TableCell>
+                            <TableCell align="center">{items.gender}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25, 100]}
+                            component="div"
+                            count={count}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                        />
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default UsersList;
