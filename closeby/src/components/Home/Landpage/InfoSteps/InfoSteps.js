ce779e07e610e2ce9ba26b0a8967a360403147fc
goldabar <bartoszgolda3@gmail.com>
2021-07-20 12:15:46 +0200

    InfoSteps with styles added

---
+++ closeby/src/components/Home/Landpage/InfoSteps/InfoSteps.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import  useStyles  from './styles';
+import ImageCard from '../ImageCard/ImageCard';
+import infos from '../../../../Static/infos';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import useWindowPosition from '../../../../Hook/useWindowPosition';
+//import { Link as Scroll } from 'react-scroll';
+
+
+const InfoSteps = () => {
+    const classes = useStyles();
+    const checked = useWindowPosition('header');
+    return (
+        <div className={classes.container}>
+        <div className={classes.root} id="info">
+        <ImageCard info={infos[0]} checked={checked}/>
+        <ArrowForwardIcon className={classes.arrowForward}/>
+        <ImageCard info={infos[1]} checked={checked}/>
+        <ArrowForwardIcon className={classes.arrowForward}/>
+        <ImageCard info={infos[2]} checked={checked}/>
+        </div>                 
+    </div>
+    
+    );
+}
+
+export default InfoSteps;
