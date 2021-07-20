c4b6204f26f32bbec2ab81f290ae2b18ada89320
goldabar <bartoszgolda3@gmail.com>
2021-07-20 13:22:22 +0200

    new graphics to infocard section added

--- closeby/src/Static/infos.js
+++ closeby/src/Static/infos.js
@@ -3,21 +3,21 @@ const infos = [
       title: 'Step 1',
       description:
         'sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate',
-      imageUrl: process.env.PUBLIC_URL + '/assets/card.jpg',
+      imageUrl: process.env.PUBLIC_URL + '/assets/user.png',
       time: 1500,
     },
     {
       title: 'Step 2',
       description:
         'sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate',
-      imageUrl: process.env.PUBLIC_URL + '/assets/card.jpg',
+      imageUrl: process.env.PUBLIC_URL + '/assets/map.png',
       time: 1500,
     },
     {
       title: 'Step 3',
       description:
         'sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate',
-      imageUrl: process.env.PUBLIC_URL + '/assets/card.jpg',
+      imageUrl: process.env.PUBLIC_URL + '/assets/finale.png',
       time: 1500,
     },
   ];
