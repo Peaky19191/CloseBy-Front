a6b6d8da867e77bfa1a0aa156fe5a5287989b094
goldabar <bartoszgolda3@gmail.com>
2021-07-20 12:16:39 +0200

    generic InfoSteps Cards added

--- closeby/src/pages/home.js
+++ closeby/src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Components/Home/Header/Header';
+import InfoSteps from '../Components/Home/Landpage/InfoSteps/InfoSteps';
 
 export const Home = () => {
     return (
         <div>
             <Header /> 
+            <InfoSteps />
         </div>
         
     )
