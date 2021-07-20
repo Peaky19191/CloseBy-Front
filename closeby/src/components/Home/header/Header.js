994c0255251d301ebe4b7119aea78194be9ed6bf
goldabar <bartoszgolda3@gmail.com>
2021-07-20 12:35:01 +0200

    Header collapse effect added

--- closeby/src/components/Home/header/Header.js
+++ closeby/src/components/Home/header/Header.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useStyles from './styles';
 import Logo from '../../../Images/logo2.jpg'
-
+import { Collapse } from '@material-ui/core';
 
 const Header = () => {
     const classes = useStyles();  
+    const [checked, setChecked] = useState(false);
+    useEffect(()=>{
+        setChecked(true);
+    },[]);
     return (
         <div className={classes.root} id="header">
+            <Collapse in={checked} {...(checked ? {timeout: 1000 } : {})} collapsedHeight={50}>
                 <div className={classes.container}>           
                     <h1 className={classes.title}>
                         Welcome to <br />                        
                         <img className={classes.logo} src={Logo} alt="website logo"></img>
                     </h1>                                        
-                </div>           
+                </div>  
+            </Collapse>         
         </div>
         
     ); 
