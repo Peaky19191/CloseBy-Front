32ef5130434f5c26cc49df3f5d505b81200c281a
goldabar <bartoszgolda3@gmail.com>
2021-07-20 13:21:44 +0200

    Navbar logo href added

--- closeby/src/components/Navbar/navbar.js
+++ closeby/src/components/Navbar/navbar.js
@@ -35,7 +35,7 @@ const Navbar = () => {
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <Grid >
-                <img className={classes.image} src="https://i.imgur.com/Tngx1R2.png" alt="logo" />
+                <a href="/"><img className={classes.image} src="https://i.imgur.com/Tngx1R2.png" alt="logo" /></a>
             </Grid>
             <Toolbar className={classes.toolbar}>
                 {currentProfile && showModeratorBoard && (
