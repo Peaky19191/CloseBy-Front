66193b5485604fa15f858dd57ca064aa45fe96af
kkopec <s15509@pjwstk.edu.pl>
2022-01-01 14:20:44 +0100

    TODO: Edit Mode Update - Clicking on Map for new Green Marker only on EditMode ON

---
+++ src/Reducers/Map/map.js
@@ -0,0 +1,15 @@
+import { SET_EDIT_MODE, } from "../../Constants/actionTypes";
+
+const initialState = {};
+
+export default function (state = initialState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case SET_EDIT_MODE:
+            return { status: payload };
+
+        default:
+            return state;
+    }
+}
