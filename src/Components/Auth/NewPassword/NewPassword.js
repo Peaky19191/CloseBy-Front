a0c089610079dcc992bb5394df5d2dd44d5a975a
goldabar <bartoszgolda3@gmail.com>
2022-01-03 19:29:10 +0100

    New password validation

--- src/Components/Auth/NewPassword/NewPassword.js
+++ src/Components/Auth/NewPassword/NewPassword.js
@@ -18,17 +18,27 @@ export const NewPassword = () => {
     const [password, setPassword] = useState("");
     const [showMessage, setShowMessage] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
+    const [confirm_password, setConfirmPassword] = useState("");
     const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState({});
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const handleShowPassword = () => setShowPassword(!showPassword);
+    const handleShowConfirmPassword = () => setShowConfirmPassword(!showConfirmPassword);
+
+    const enabled = password.length > 0;
 
     const { token } = useParams();
 
     const dispatch = useDispatch();
 
-    const handleShowPassword = () => setShowPassword(!showPassword);
-
     const onChangePassword = (e) => {
         const password = e.target.value;
         setPassword(password);
+    };
+
+    const onChangeConfirmPassword = (e) => {
+        const confirm_password = e.target.value;
+        setConfirmPassword(confirm_password);
     };
 
     const handleSubmit = (e) => {
@@ -36,16 +46,29 @@ export const NewPassword = () => {
         setShowMessage(false);
         setLoading(true);
 
+        if (validate())
         dispatch(setNewPassword(password, token))
-            .then(() => {
-                setShowMessage(true);
-                setLoading(false);
-            })
-            .catch(() => {
-                setShowMessage(true);
-                setLoading(false);
-            });
+        .then(() => {
+            setShowMessage(true);
+            setLoading(false);
+
+        })
+        .catch(() => {
+            setShowMessage(true);
+            setLoading(false);
+
+        });
     };
+
+    const validate = () => {
+        let temp = {}
+        temp.password = (/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*ąćęłńóśźżĄĘŁŃÓŚŹŻ]{6,50}$/).test(password) ? "" : "At least 6 characters required including one number and one special character"
+        temp.confirm_password = confirm_password == password ? "" : "Passwords are not the same"
+        setErrors({
+            ...temp
+        })
+        return Object.values(temp).every(x => x == "");
+    }
 
     return (
         <Container className={classes.container} component="main" maxWidth="xs">
@@ -60,16 +83,28 @@ export const NewPassword = () => {
                 <form className={classes.form} onSubmit={handleSubmit} >
                     <Grid container spacing={2}>
                         <Grid item xs={12} >
-                            <TextField InputProps={{
-                                endAdornment: (
-                                    <InputAdornment position="end">
-                                        <IconButton onClick={handleShowPassword}>
-                                            {!showPassword ? <Visibility /> : <VisibilityOff />}
-                                        </IconButton>
-                                    </InputAdornment>
-                                )
-                            }}
-                                fullWidth name="password" htmlFor="password" type="password" label="Password" value={password} onChange={onChangePassword} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} variant="outlined" item />
+                        <TextField
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton onClick={handleShowPassword}>
+                                                {!showPassword ? <Visibility /> : <VisibilityOff />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    )
+                                }} error={errors.password} helperText={(errors.password)} label="Password" name="password" htmlFor="password" type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} variant="outlined" fullWidth value={password} onChange={onChangePassword} />
+                        </Grid>
+                        <Grid item xs={12} >
+                            <TextField disabled={!enabled}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton onClick={handleShowConfirmPassword}>
+                                                {!showConfirmPassword ? <Visibility /> : <VisibilityOff />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    )
+                                }} type={showConfirmPassword ? 'text' : 'password'} handleShowConfirmPassword={handleShowConfirmPassword} variant="outlined" fullWidth value={confirm_password} error={errors.confirm_password} helperText={(errors.confirm_password)} label="Confirm password" name="confirm_password" htmlFor="confirm_password" variant="outlined" fullWidth value={confirm_password} onChange={onChangeConfirmPassword} />
                         </Grid>
                     </Grid>
                     <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
