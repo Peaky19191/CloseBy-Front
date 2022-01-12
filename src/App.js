378d451841ace20a6107647a90a15901d0b18747
goldabar <bartoszgolda3@gmail.com>
2022-01-12 16:52:50 +0100

    Toasts container

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Router } from "react-router-dom";
 import { history } from "./Helpers/history";
 import Navbar from './Components/Navbar/Navbar';
 import SwitchComponent from './Components/Switch/Switch';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   const classes = useStyles();
@@ -14,6 +16,10 @@ const App = () => {
   return (
     <Router history={history}>
       <div className={classes.root}>
+        <ToastContainer 
+          position="bottom-right"
+          autoclose={2500}
+        />
         <Navbar />
         <SwitchComponent />
       </div>
