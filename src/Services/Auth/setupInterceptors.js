4971d60bd26fd19993e4ddd462165ee2b4f69071
kkopec <s15509@pjwstk.edu.pl>
2022-02-13 18:30:53 +0100

    Cleaning code

--- src/Services/Auth/setupInterceptors.js
+++ src/Services/Auth/setupInterceptors.js
@@ -24,23 +24,9 @@ const setup = (store, history) => {
         },
         async (err) => {
             const originalConfig = err.config;
-            console.log("originalConfig")
-            console.log(originalConfig)
-            console.log("err.response")
-            console.log(err.response)
-            console.log("originalConfig.url")
-            console.log(originalConfig.url)
-
             if (originalConfig.url !== "/identity/refresh-access-token" && originalConfig.url !== "/identity/login" && err.response) {
-                console.log("err.response.status")
-                console.log(err.response.status)
-                console.log("originalConfig._retry")
-                console.log(originalConfig._retry)
-
                 // Access Token was expired
                 if (err.response.status === 401 && !originalConfig._retry) {
-                    console.log("2if")
-
                     originalConfig._retry = true;
                     try {
                         const rs = await axiosInstance.put("/identity/refresh-access-token", {
@@ -58,32 +44,13 @@ const setup = (store, history) => {
 
                         return axiosInstance(originalConfig);
                     } catch (_error) {
-                        console.log("111")
-                        console.log(_error)
-                        console.log(_error.response)
-
-                        // window.location.reload();
-
                         dispatch(logout());
-
                         history.push('/login');
-                        // window.location.reload();
 
                         return Promise.reject(_error);
                     }
                 }
-                console.log("2If not")
             }
-            console.log("222")
-            console.log(err)
-            console.log(err.response)
-            // history.push('/login');
-            // window.location.reload();
-            // dispatch(logout());
-            // dispatch(clearMessage());
-
-            // <Redirect to="/eventList" /> 
-
             return Promise.reject(err);
         }
     );
